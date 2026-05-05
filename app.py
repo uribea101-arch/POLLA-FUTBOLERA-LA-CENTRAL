eb38ec0ce545da916cf72e04c7221ff598219a5c
uribea101-arch <uribea101@gmail.com>
2026-05-05 15:18:47 -0500

    Refactor admin button styles and markup
    Removed admin button styling and associated HTML.

--- app.py
+++ app.py
@@ -46,24 +46,15 @@ st.markdown(
 
 st.markdown("""
 <style>
-.admin-btn {
-    position: absolute;
-    top: 35px;      /* ajusta si lo quieres más arriba/abajo */
-    right: 20px;    /* pegado a la derecha del título */
-    width: 25px;
-    height: 25px;
+div[data-testid="stButton"] > button {
     background-color: transparent;
+    color: transparent;
     border: none;
-    cursor: pointer;
+}
+div[data-testid="stButton"] > button:hover {
+    background-color: transparent;
 }
 </style>
-
-<div style="position: relative;">
-    <h1 style='text-align: center;'>⚽ ¡En La Central, el Mundial se vive mejor!</h1>
-    <form action="" method="get">
-        <button name="admin" value="1" class="admin-btn"></button>
-    </form>
-</div>
 """, unsafe_allow_html=True)
 
 col1, col2, col3 = st.columns([10,1,1])
