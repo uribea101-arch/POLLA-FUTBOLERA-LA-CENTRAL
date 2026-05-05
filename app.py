df7b3a678fea8eb3c3016d4bd44b9d9f14b69525
uribea101-arch <uribea101@gmail.com>
2026-05-05 17:57:08 -0500

    Change flag image for Equipo 2 from AR to PT

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ with col1:
 with col2:
     st.markdown(f"""
     <div style='text-align:center;'>
-        <img src='https://flagcdn.com/w40/ar.png'><br>
+        <img src='https://flagcdn.com/w40/pt.png'><br>
         <b>{equipo2}</b>
     </div>
     """, unsafe_allow_html=True)
